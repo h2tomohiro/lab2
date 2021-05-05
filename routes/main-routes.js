e97028c3e62b5b6cc88f97bee7993c9ce647c24f
h2tomohiro <h2.adachi.tomohiro@gmail.com>
2021-05-05 13:45:08 -0700

    Fix title

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -8,18 +8,18 @@ let data = fs.readFileSync("notes.json");
 let notes = JSON.parse(data);
 
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { title: "Guestbook" });
 });
 
 router.get("/read", (req, res, next) => {
-  res.render("read", { notes: notes });
+  res.render("read", { notes: notes, title: "Read notes" });
 });
 
 router.get("/leave", (req, res, next) => {
-  res.render("leave");
+  res.render("leave", { title: "Leave notes" });
 });
 
-router.post("/add-note", (req, res, next) => {
+router.post("/add", (req, res, next) => {
   notes.push({
     id: Math.random(),
     note: req.body.note,
