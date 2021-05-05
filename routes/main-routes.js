ed0ee6b67e26a3d764e2d6ed71237d162513cb2f
Francois Polo <andasan@gmail.com>
2021-05-05 10:46:53 -0700

    added a writeFile for form submission

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const fs = require('fs')
+const path = require('path')
 
 const router = express.Router()
 
-const data = [
-    {
-        id: 1,
-        product: "Shoes"
-    },
-    {
-        id: 2,
-        product: "Bread"
-    }
-]
+const data = []
 
 router.get('/', (req,res,next) => {
     res.render('index', { products: data, title: "Shopping List" })
+})
+
+router.post('/add-product', (req,res,next) => {
+    data.push({
+        id: Math.random(),
+        product: req.body.product
+    })
+
+    fs.writeFile(path.join(__dirname, '..', 'products.json'), JSON.stringify(data, null, 2), () => {
+        res.status(302).redirect('/')
+    })
 })
 
 module.exports = router
